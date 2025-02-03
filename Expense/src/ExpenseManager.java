ce03cfcb9298ae1f6c2203b5a7e80aaf9b4cfe00
Chanrat <Chanrat.sophal@student.cadt.edu.kh>
2025-02-03 21:36:35 +0700

    update

--- Expense/src/ExpenseManager.java
+++ Expense/src/ExpenseManager.java
@@ -1,3 +1,19 @@
-public class ExpenseManager {
+import java.util.ArrayList;
 
+public class ExpenseManager {
+    // Class scope
+    private ArrayList<Expense> expenseList;
+
+    // Class Constructor 
+    public ExpenseManager() {
+        expenseList = new ArrayList<>();
+    }
+
+    public void addExpense(String category, double amount, String date) {
+    
+    }
+
+    public void viewExpenses() {
+        
+    }
 }
